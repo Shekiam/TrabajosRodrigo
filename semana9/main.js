4c9064f56c9b726f7e8d49e8d3e6f278d2359a10
Shekiam <foxotakuko@gmail.com>
2021-05-18 11:00:23 -0500

    trabajosSemana9

---
+++ semana9/main.js
@@ -0,0 +1,68 @@
+let nombre = "Rodrigo Gonzalez Escalante";
+
+let edad = 18;
+
+let hombre = true;
+
+let nacimiento = 2021 -18;
+
+let cumpleaños = "04/07/2003";
+
+
+
+var a = 1475;
+
+var b = 7489;
+
+function sum (a , b){
+    console.log(a+b)
+}
+
+
+
+
+var c = 1897;
+
+var d = 4721;
+
+function subs (c , d){
+    console.log(c-d)
+}
+
+
+
+
+var edad2 = 17
+
+var mayoriaDeEdad = 18
+
+function mayor (edad2 , mayoriaDeEdad){
+    if (edad >= mayoriaDeEdad){
+        console.log("Mayor de edad")
+    }
+    else{
+        console.log("Menor de edad")
+    }
+}
+
+
+var number = -3
+
+let cero = 0
+
+function evaluate (number , cero){
+    if (number > cero){
+        console.log("Positivo")
+    }else if (number < cero){
+        console.log("Negativo")
+    }else if (number = cero){
+        console.log("Cero")
+    }
+}
+
+
+var pares = [''];
+
+for (pares=0; pares<=100; pares=pares +1 *2) {
+    console.log(pares)
+}
